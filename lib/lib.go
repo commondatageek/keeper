6c86bf3ea9ffca9dec6c1f20c0ec4fe7d63ea503
aaron-johnson <aaron-johnson@pluralsight.com>
2021-06-18 23:28:50 -0600

    Add local storage backend

--- lib/lib.go
+++ lib/lib.go
@@ -2,11 +2,16 @@ package lib
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 	"time"
 )
+
+const ioCopyBufferSize = 4_194_304
+
+type ItemList []interface{}
 
 func UnixTimeNow() int64 {
 	return time.Now().Unix()
@@ -38,4 +43,61 @@ func SafeWriteFile(filename string, data io.Reader) (int64, error) {
 	}
 
 	return writeN, nil
+}
+
+type DatabaseWriter interface {
+	Write(ItemList) (n int64, err error)
+}
+
+type DatabaseReader interface {
+	Read() (ItemList, error)
+}
+
+type LocalDatabase struct {
+	Path string
+}
+
+func NewLocalDatabase(path string) *LocalDatabase {
+	return &LocalDatabase{
+		Path: path,
+	}
+}
+
+func (d *LocalDatabase) Write(database ItemList) (n int64, err error) {
+	reader, writer := io.Pipe()
+	bufReader := bufio.NewReaderSize(reader, ioCopyBufferSize)
+	bufWriter := bufio.NewWriterSize(writer, ioCopyBufferSize)
+	done := make(chan error)
+
+	// asynchronous write to file
+	go func() {
+		_, writeErr := SafeWriteFile(d.Path, bufReader)
+		done <- writeErr
+	}()
+
+	// while we stream each item
+	for _, x := range database {
+		jsonBytes, marshalError := json.Marshal(x)
+		if marshalError != nil {
+			return n, marshalError
+		}
+		_, writeErr := bufWriter.Write(jsonBytes)
+		if writeErr != nil {
+			return n, writeErr
+		}
+		_, writeErr = bufWriter.Write([]byte{'\n'}) // newline between each record
+		if writeErr != nil {
+			return n, writeErr
+		}
+		n++ // keep track of the number of items we've successfully written
+	}
+	bufWriter.Flush()
+	writer.Close()
+
+	result := <-done
+	return n, result
+}
+
+func (d *LocalDatabase) Read() error {
+	panic("LocalDatabase.Read() not yet implemented")
 }
